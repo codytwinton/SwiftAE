9fc52039f419ccdc07b894f2ceb15e799a2bf949
Cody Winton <cody.t.winton@gmail.com>
2018-02-10 20:30:35 -0600

    Updated Coding Examples

--- CodingExamples.playground/Pages/PowerSet.xcplaygroundpage/Contents.swift
+++ CodingExamples.playground/Pages/PowerSet.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
 The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
 
 For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
-For example, given the string "abc", it should return ["", "a", {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+For example, given the string "abc", it should return ["", "a", "b", "c", "ab", "ac", "bc", "abc"].
 
 You may also use a list or array to represent a set.
 */
@@ -16,11 +16,22 @@ extension String {
 
 	var powerSet: [String] {
 		var set: [String] = [""]
+
+		for el in self {
+			var elSet: [String] = []
+
+			for previous in set {
+				elSet.append(previous + String(el))
+			}
+
+			set.append(contentsOf: elSet)
+		}
+
 		return set
 	}
 }
 
-// Tests
+// Test Structure
 
 struct TestValues {
 	var input: String
@@ -32,14 +43,17 @@ var tests: [TestValues] = [
 	TestValues(input: "abc",  result: ["", "a", "b", "c", "ab", "ac", "bc", "abc"])
 ]
 
+// Tests
+
+let date = Date()
+
+print("Begin Tests: \(date.timeIntervalSinceNow)\n\n----------------\n")
+
 for (index, test) in tests.enumerated() {
 	let result = test.input.powerSet
 
-	switch result.sorted() == test.result.sorted() {
-	case false:
-		print("\n\nFailed test \(index):\n\tinput: \(test.input)\n\texpected: \(test.result)\n\tactual: \(result)")
-	case true:
-		print("\n\nPassed test \(index). result: \(result)")
-	}
+	let wasSuccess = result.sorted() == test.result.sorted()
+	print("\(wasSuccess ? "Passed" : "Failed") Test \(index):\n\tinput: \(test.input)\n\texpected: \(test.result)\n\tactual: \(result)\n")
 }
 
+print("\n----------------\n\nEnd Tests: \(date.timeIntervalSinceNow)")
