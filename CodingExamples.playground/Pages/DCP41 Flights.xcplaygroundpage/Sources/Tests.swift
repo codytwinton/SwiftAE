ca50dcdee13d9017d822bac76b629db265ccd6b4
Cody Winton <cody.t.winton@gmail.com>
2018-02-14 17:04:56 -0600

    Added DCP41 Flights. Building, but failing tests.

---
+++ CodingExamples.playground/Pages/DCP41 Flights.xcplaygroundpage/Sources/Tests.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public struct TestData: Testable {
+
+	public static let tests: [TestData] = [
+		TestData(input: (start: "YUL", flights: [("SFO", "HKO"), ("YYZ", "SFO"), ("YUL", "YYZ"), ("HKO", "ORD")]), expected: ["YUL", "YYZ", "SFO", "HKO", "ORD"]),
+		TestData(input: (start: "COM", flights: [("SFO", "COM"), ("COM", "YYZ")]), expected: nil),
+		TestData(input: (start: "A", flights: [("A", "B"), ("A", "C"), ("B", "C"), ("C", "A")]), expected: ["A", "B", "C", "A", "C"]),
+	]
+
+	public let input: (start: String, flights: [(origin: String, destination: String)])
+	public let expected: [String]?
+
+	public func assert(with actual: [String]?) {
+		let success: Bool
+
+		if let actual = actual, let expected = expected {
+			success = actual == expected
+		} else {
+			success = actual == nil && expected == nil
+		}
+
+		log(success: success, actual: actual)
+	}
+}
