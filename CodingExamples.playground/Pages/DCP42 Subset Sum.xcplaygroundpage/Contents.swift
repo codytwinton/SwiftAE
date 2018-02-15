5c61e3e0b671079059826bf314d364862f996d6c
Cody Winton <cody.t.winton@gmail.com>
2018-02-15 16:58:28 -0600

    Refactored DCP42

--- CodingExamples.playground/Pages/DCP42 Subset Sum.xcplaygroundpage/Contents.swift
+++ CodingExamples.playground/Pages/DCP42 Subset Sum.xcplaygroundpage/Contents.swift
@@ -14,10 +14,11 @@ import Foundation
 extension Array where Iterator.Element == Int {
 
 	public func sumSubset(for target: Int) -> [Int] {
-		guard !isEmpty else { return self }
+		var sum = reduce(0, +)
+
+		guard sum > target else { return sum == target ? self : [] }
 
 		var vals = sorted { $0 > $1 }
-		var sum = vals.reduce(0, +)
 
 		while sum > target {
 
@@ -40,6 +41,9 @@ extension Array where Iterator.Element == Int {
 
 // MARK: Tests
 
+let testDate = Date()
+print("Tests Started\n\n---\n")
+
 for test in TestData.tests {
 	// Arrange
 	let input = test.input
@@ -50,3 +54,5 @@ for test in TestData.tests {
 	// Assert
 	test.assert(with: actual)
 }
+
+print("---\n\nTests Ended:\n\telapsed: \(Date().timeIntervalSince(testDate))")
