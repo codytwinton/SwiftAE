89f98fd7d59149d13d4a429e397c7f937b0ee3bc
Cody Winton <cody.t.winton@gmail.com>
2018-02-17 16:10:07 -0600

    Added Project for testing with Objective C

---
+++ CodingExamplesObjC/CodingExamplesObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CodingExamplesObjC
+//
+//  Created by Cody Winton on 2/17/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
