af658b96b794ea63c54d28a1bc39bdb8df297e1a
Cody Winton <cody.t.winton@gmail.com>
2018-02-17 20:27:54 -0600

    Added CodingExamples Project

---
+++ CodingExamples/CodingExamples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodingExamples
+//
+//  Created by Cody Winton on 2/17/18.
+//  Copyright © 2018 Cody Winton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
