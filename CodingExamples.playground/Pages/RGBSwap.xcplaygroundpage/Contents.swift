088028cb9e6d1e356c1e8ed064fce050e9222b29
Cody Winton <cody.t.winton@gmail.com>
2018-02-18 21:42:28 -0600

    Minor updates

--- CodingExamples.playground/Pages/RGBSwap.xcplaygroundpage/Contents.swift
+++ CodingExamples.playground/Pages/RGBSwap.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 Given an array of strictly the characters "R", "G", and "B", segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
 
-Do this in linear time and in-place.
+Do this in linear time and in-place (i.e. O(n) time and O(1) space).
 
 For example, given the array ["G", "B", "R", "R", "B", "R", "G"], it should become ["R", "R", "R", "G", "G", "B", "B"].
 */
