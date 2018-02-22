6f8f8349b702546227ceeda090262d9195130c34
Cody Winton <cody.t.winton@gmail.com>
2018-02-22 17:14:51 -0600

    Updated the tests

--- CodingExamples.playground/Pages/DCP48 Reconstruct Tree.xcplaygroundpage/Sources/Tests.swift
+++ CodingExamples.playground/Pages/DCP48 Reconstruct Tree.xcplaygroundpage/Sources/Tests.swift
@@ -6,16 +6,22 @@ import Foundation
   1   2
  / \ / \
 3  4 5  6
+
+    7
+   / \
+  2   10
+ / \ / \
+1  5 9  11
 */
 
 public struct TestData: Testable {
 
 	public static let tests: [TestData] = [
-		TestData(input: (preOrder: [], inOrder: []), expected: BinaryNode(value: 0)),
+		TestData(input: (preOrder: [6], inOrder: [6]), expected: BinaryNode(value: 6)),
 		TestData(input: (preOrder: [0, 1, 3, 4, 2, 5, 6], inOrder: [3, 1, 4, 0, 5, 2, 6]), expected: expectedNode)
 	]
 
-	public static var expectedNode: BinaryNode<Int> {
+	public static let expectedNode: BinaryNode<Int> = {
 
 		let node = BinaryNode(value: 0)
 		node.left = BinaryNode(value: 1)
@@ -28,7 +34,7 @@ public struct TestData: Testable {
 		node.right?.right = BinaryNode(value: 6)
 
 		return node
-	}
+	}()
 
 	public var input: (preOrder: [Int], inOrder: [Int])
 	public let expected: BinaryNode<Int>
