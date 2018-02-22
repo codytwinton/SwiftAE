762885db9314b4cbc8d996352962e3c92af19ea2
Cody Winton <cody.t.winton@gmail.com>
2018-02-22 17:14:35 -0600

    Updated the BinaryNode

--- CodingExamples.playground/Sources/BinaryNode.swift
+++ CodingExamples.playground/Sources/BinaryNode.swift
@@ -2,25 +2,17 @@ import Foundation
 
 public class BinaryNode<T>: CustomStringConvertible {
 	public let value: T
-
 	public var left: BinaryNode?
 	public var right: BinaryNode?
 
 	public var description: String {
-
 		var leftStr = "nil"
-
-		if let left = left {
-			leftStr = "\t\(left)"
-		}
+		if let left = left { leftStr = "\(left)" }
 
 		var rightStr = "nil"
+		if let right = right { rightStr = "\(right)" }
 
-		if let right = right {
-			rightStr = "\t\(right)"
-		}
-
-		return "BinaryNode(value: \(value), left: \(leftStr), right: \(rightStr))"
+		return "\(value), \(leftStr), \(rightStr)"
 	}
 
 	public init(value: T) {
