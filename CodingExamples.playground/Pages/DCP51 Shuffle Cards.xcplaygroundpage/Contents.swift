c480dca33b7b70e9f63f2190f87fb36f6c1d51d5
Cody Winton <cody.t.winton@gmail.com>
2018-02-24 14:22:04 -0600

    Shuffled cards

--- CodingExamples.playground/Pages/DCP51 Shuffle Cards.xcplaygroundpage/Contents.swift
+++ CodingExamples.playground/Pages/DCP51 Shuffle Cards.xcplaygroundpage/Contents.swift
@@ -11,13 +11,39 @@ import Foundation
 
 // MARK: Solution
 
+extension CountableClosedRange where Bound == Int {
+
+	func random() -> Int {
+		let range = upperBound - lowerBound
+		let rand = arc4random_uniform(UInt32(range))
+		return Int(rand) + lowerBound
+	}
+
+	func random(seed: Int) -> Int {
+		guard seed < upperBound else { return random() }
+
+		let seedRange = lowerBound...seed
+
+		let permutations: Int = upperBound / seed + 1
+		var randMax: Int = (permutations * 2) * seedRange.random() + seedRange.random() - (permutations * 2)
+
+		guard randMax < upperBound * 2 else { return random(seed: seed) }
+
+		let rand = randMax % upperBound
+
+		guard rand > 0 else { return random(seed: seed) }
+		return rand - 1
+	}
+}
+
 extension Array where Iterator.Element == Int {
 
 	mutating func shuffle(seed: Int) {
 		var indexesLeft: Set = Set(enumerated().map { $0.offset })
+		let cardRange = 1...52
 
 		while let index = indexesLeft.first {
-			let rand: Int = Int(arc4random_uniform(UInt32(seed)))
+			let rand = cardRange.random(seed: seed)
 			swapAt(index, rand)
 			indexesLeft.remove(index)
 		}
@@ -34,7 +60,14 @@ let input = Array(1...52)
 
 // Act
 var actual = input
-actual.shuffle(seed: 51)
+actual.shuffle(seed: 5)
+
+for (index, item) in input.enumerated() {
+
+	if item == actual[index] {
+		print("\(item) was not shuffled and is: \(actual[index])")
+	}
+}
 
 // Assert
 print("""
