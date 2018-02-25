b84695d151c9f905091bd6340dc70ceefdbbc43d
Cody Winton <cody.t.winton@gmail.com>
2018-02-25 14:33:12 -0600

    LRU Cache working at O(n) time. Need to reduce to O(1).

--- CodingExamples.playground/Pages/DCP52 LRU Cache.xcplaygroundpage/Contents.swift
+++ CodingExamples.playground/Pages/DCP52 LRU Cache.xcplaygroundpage/Contents.swift
@@ -5,6 +5,8 @@ Implement an LRU (Least Recently Used) cache. It should be able to be initialize
 set(key, value): sets key to value. If there are already n items in the cache and we are adding a new item, then it should also remove the least recently used item.
 get(key): gets the value at key. If no such key exists, return null.
 Each operation should run in O(1) time.
+
+Assuming that setting a key,value pair updates the most recently used as well
 */
 
 import Foundation
@@ -14,17 +16,40 @@ import Foundation
 struct LRUCache<T> {
 
 	let capacity: Int
-	private var items: [String: T] = [:]
+	private(set) var items: [String: T] = [:]
+	private(set) var itemKeys: [String] = []
 
 	init(capacity: Int) {
 		self.capacity = capacity
 	}
 
 	mutating func set(_ value: T, for key: String) {
-		self.items[key] = value
+		let didExist = items[key] != nil
+		items[key] = value
+
+		if didExist {
+			for (index, itemKey) in itemKeys.enumerated() {
+				guard key == itemKey, index != 0 else { continue }
+				itemKeys.remove(at: index)
+				break
+			}
+		}
+
+		itemKeys.insert(key, at: 0)
+		guard itemKeys.count > capacity else { return }
+		let key = itemKeys.last ?? ""
+		items.removeValue(forKey: key)
+		itemKeys.removeLast()
 	}
 
-	func get(for key: String) -> T? {
+	mutating func get(for key: String) -> T? {
+		for (index, itemKey) in itemKeys.enumerated() {
+			guard key == itemKey, index != 0 else { continue }
+			itemKeys.remove(at: index)
+			itemKeys.insert(key, at: 0)
+			break
+		}
+
 		return items[key]
 	}
 }
@@ -40,6 +65,33 @@ for test in TestData.tests {
 	var cache = LRUCache<Int>(capacity: 10)
 
 	// Act
+
+	let nums = [1, 3, 2, 5, 4, 7, 6, 9, 8, 10, 12, 11]
+	let numKeys = nums.filter {$0 != 1 && $0 != 3}.map {"\($0)"}
+
+	for i in [1, 3, 2, 5, 4, 7, 6, 9, 8, 10, 12, 11] {
+		cache.set(i, for: "\(i)")
+		print("itemKeys \(cache.itemKeys)")
+	}
+
+	if cache.itemKeys == numKeys.reversed() {
+		print("Worked here")
+	}
+
+	for i in Array(1...12) {
+		let val = cache.get(for: "\(i)")
+
+		print("cache.itemKeys after i fetch \(i) \(cache.itemKeys)")
+
+		if i == 1 || i == 3, val == nil {
+			print("worked for deletion")
+		}
+
+		if let val = val {
+			print("val valid for i \(i)")
+		}
+	}
+
 	//let actual = handleProblem(for: input)
 
 	// Assert
