94a1db5b0fd9d7f98e3e5e5917f6904dcf5d03bd
Cody Winton <cody.t.winton@gmail.com>
2018-02-26 15:37:12 -0600

    Added Queue and Stack to Coding Examples

---
+++ CodingExamples.playground/Sources/Queue.swift
@@ -0,0 +1,47 @@
+public protocol Queueable {
+	associatedtype Element
+
+	var count: Int { get }
+	var isEmpty: Bool { get }
+	var items: [Element] { get }
+
+	func peek() -> Element?
+
+	mutating func clear()
+	mutating func enqueue(_ item: Element?)
+	mutating func dequeue() -> Element?
+
+	init(_ items: [Element])
+}
+
+public struct Queue<T>: Queueable {
+
+	public typealias Element = T
+
+	public var count: Int { return items.count }
+	public var isEmpty: Bool { return items.isEmpty }
+
+	public private(set) var items: [T]
+
+	public func peek() -> T? {
+		return items.first
+	}
+
+	public mutating func clear() {
+		items.removeAll()
+	}
+
+	public mutating func enqueue(_ item: T?) {
+		guard let item = item else { return }
+		items.append(item)
+	}
+
+	public mutating func dequeue() -> T? {
+		guard !items.isEmpty else { return nil }
+		return items.removeFirst()
+	}
+
+	public init(_ items: [T] = []) {
+		self.items = items
+	}
+}
---
+++ CodingExamples.playground/Sources/Stack.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+public protocol Stackable {
+	associatedtype Element
+
+	var count: Int { get }
+	var isEmpty: Bool { get }
+	var items: [Element] { get }
+
+	func peek() -> Element?
+
+	mutating func clear()
+	mutating func push(_ item: Element?)
+	mutating func pop() -> Element?
+
+	init(_ items: [Element])
+}
+
+public struct Stack<T>: Stackable {
+
+	public typealias Element = T
+
+	public var count: Int { return items.count }
+	public var isEmpty: Bool { return items.isEmpty }
+
+	public private(set) var items: [T]
+
+	public func peek() -> T? {
+		return items.last
+	}
+
+	public mutating func clear() {
+		items.removeAll()
+	}
+
+	public mutating func push(_ item: T?) {
+		guard let item = item else { return }
+		items.append(item)
+	}
+
+	public mutating func pop() -> T? {
+		guard !items.isEmpty else { return nil }
+		return items.removeLast()
+	}
+
+	public init(_ items: [T] = []) {
+		self.items = items
+	}
+}
