a8905bd8867eb4f959e6a0fc37b7b62650cca287
Cody Winton <cody.t.winton@gmail.com>
2018-03-03 16:48:05 -0600

    Added tests that are failing

--- CodingExamples.playground/Pages/DCP58 Rotated Sorted.xcplaygroundpage/Sources/Tests.swift
+++ CodingExamples.playground/Pages/DCP58 Rotated Sorted.xcplaygroundpage/Sources/Tests.swift
@@ -13,6 +13,7 @@ public struct TestData: Testable {
 		TestData(input: (array: [13, 18, 25, 2, 8, 10], element: 8), expected: 4),
 		TestData(input: (array: [13, 18, 25, 2, 8, 10], element: 10), expected: 5),
 		TestData(input: (array: [13, 18, 25, 2, 8, 10], element: 7), expected: -1),
+		TestData(input: (array: Array(50...100) + Array(1...49), element: 11), expected: 5),
 	]
 
 	public var input: (array: [Int], element: Int)
