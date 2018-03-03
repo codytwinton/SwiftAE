23ecb34a229d99f52fd77a28c6fe9ad6b88e0d07
Cody Winton <cody.t.winton@gmail.com>
2018-03-03 14:56:02 -0600

    Added DCP58

---
+++ CodingExamples.playground/Pages/DCP58 Rotated Sorted.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+/* This problem was asked by Amazon.
+
+An sorted array of integers was rotated an unknown number of times.
+
+Given such an array, find the index of the element in the array in faster than linear time. If the element doesn't exist in the array, return null.
+
+For example, given the array [13, 18, 25, 2, 8, 10] and the element 8, return 4 (the index of 8 in the array).
+
+You can assume all the integers in the array are unique.
+*/
+
+import Foundation
+
+// MARK: Solution
+
+func handleProblem(for input: String) -> String {
+	return "Actual"
+}
+
+// MARK: Tests
+
+let testDate = Date()
+print("Tests Started\n\n---\n")
+
+for test in TestData.tests {
+	// Arrange
+	let input = test.input
+
+	// Act
+	let actual = handleProblem(for: input)
+
+	// Assert
+	test.assert(with: actual)
+}
+
+print("---\n\nTests Ended:\n\telapsed: \(Date().timeIntervalSince(testDate))")
